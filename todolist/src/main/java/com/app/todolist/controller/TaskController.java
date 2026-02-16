115de3210bee10b713d89bdcb621b65079f5e6db
cesarmilan16 <cesar3bfermi@gmail.com>
2026-02-16 21:02:27 +0100

    fix controller

--- todolist/src/main/java/com/app/todolist/controller/TaskController.java
+++ todolist/src/main/java/com/app/todolist/controller/TaskController.java
@@ -1,13 +1,10 @@
 package com.app.todolist.controller;
 
-import com.app.todolist.models.Task;
 import com.app.todolist.services.TaskService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/tasks")
